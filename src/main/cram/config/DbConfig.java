f832ca7f5bd37501f41f15f4734c102532f1885b
Yuom Kitya <kityayuom@gmail.com>
2025-08-18 23:06:54 +0700

    add config

--- src/main/cram/config/DbConfig.java
+++ src/main/cram/config/DbConfig.java
@@ -1,4 +1,20 @@
 package main.cram.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DbConfig {
+    private final Properties prop = new Properties();
+    public DbConfig() {
+        try(InputStream in = getClass().getResourceAsStream("application.properties")) {
+            if(in != null) prop.load(in);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public String url() {return prop.getProperty("db.url", "");}
+    public String username() {return prop.getProperty("db.user", "");}
+    public String password() {return prop.getProperty("db.password", "");}
 }
--- src/main/cram/view/MenuView.java
+++ src/main/cram/view/MenuView.java
@@ -1,5 +1,15 @@
 package main.cram.view;
 
 public class MenuView {
+    public void showWelcome() {
+        System.out.println();
+        System.out.println("======================================");
+
+        System.out.println("  Welcome to CRAM (Console Edition)   ");
+        System.out.println("======================================");
+
+
+
+    }
 
 }
